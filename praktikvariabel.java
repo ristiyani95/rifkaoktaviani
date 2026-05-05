80ba300289626ae5c6399698d99da6d223b49cce
rifkajeon6-glitch <rifkajeon6@gmail.com>
2026-05-05 10:55:32 +0700

    Add files via upload

---
+++ praktikvariabel.java
@@ -0,0 +1,44 @@
+class praktikvariabel {
+    public static void main(String[] args) {
+        System.out.println("----Curriculum Vitae----");
+        
+      String nama = "Ristiyani";
+      String hobbi ="Listen musik";
+      int umur = 17;
+      double nilaiiq = 150.0;
+      boolean status = true;
+      
+      
+        
+       
+        System.out.println("Nama  :" + nama);
+        System.out.println("umur  :" + umur);
+        System.out.println("hobbi :" + hobbi);
+        System.out.println("nilaiiq :"+ nilaiiq);
+        System.out.println("status :"+ status);
+        
+        
+        System.out.println("update nilai iq baru : 145.0");
+        System.out.println("----------------------------");
+        
+      System.out.println("----Curriculum Vitae----");
+          
+        String nama1 = "Rifka";
+      String hobi ="Read book";
+      int umur2 = 18;
+      double nilai = 150.0;
+      boolean statuss = true;
+      
+      System.out.println("Nama  :" + nama1);
+        System.out.println("umur  :" + umur2);
+        System.out.println("hobi :" + hobi);
+        System.out.println("nilai :"+ nilai);
+        System.out.println("statuss :"+ statuss);
+        
+         System.out.println("update nilai iq baru : 140.0");
+        System.out.println("----------------------------");
+        
+      
+        
+    }
+}
